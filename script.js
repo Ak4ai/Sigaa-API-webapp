5deec3929426cd8327435ee33585acc969803147
Ak4ai <129908980+Ak4ai@users.noreply.github.com>
2026-03-07 21:23:33 -0300

    revert: volta evento de clicar fora para versão anterior

--- script.js
+++ script.js
@@ -1462,9 +1462,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Fecha se clicar fora
   document.addEventListener('click', (e) => {
-    if (!fab.contains(e.target) && !fabMinimized.contains(e.target)) {
-      closeFab();
-    }
+    if (!fab.contains(e.target)) closeFab();
   });
 
   // Fecha em resize para evitar menus abertos em desktop
